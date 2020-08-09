b2fcab9227634bdce4bb75f70537693cd2b5b4a0
David Cantrell <dcantrell@redhat.com>
2020-08-09 12:36:14 -0400

    WIP: GitHub Action files for CI of rpminspect on Alpine Linux

---
+++ osdeps/alpine/defs.mk
@@ -0,0 +1,2 @@
+PKG_CMD = apk add
+PIP_CMD = pip3 install
---
+++ osdeps/alpine/post.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+PATH=/bin:/usr/bin:/sbin:/usr/sbin
+CWD="$(pwd)"
+
+# There mandoc package in Alpine Linux lacks the library
+curl -O https://mandoc.bsd.lv/snapshots/mandoc.tar.gz
+SUBDIR="$(tar -tvf mandoc.tar.gz | head -n 1 | rev | cut -d ' ' -f 1 | rev)"
+tar -xvf mandoc.tar.gz
+cd ${SUBDIR}
+echo 'PREFIX=/usr/local'                     > configure.local
+echo 'BINDIR=/usr/local/bin'                >> configure.local
+echo 'SBINDIR=/usr/local/sbin'              >> configure.local
+echo 'MANDIR=/usr/local/share/man'          >> configure.local
+echo 'INCLUDEDIR=/usr/local/include'        >> configure.local
+echo 'LIBDIR=/usr/local/lib'                >> configure.local
+echo 'LN="ln -sf"'                          >> configure.local
+echo 'MANM_MANCONF=mandoc.conf'             >> configure.local
+echo 'INSTALL_PROGRAM="install -D -m 0755"' >> configure.local
+echo 'INSTALL_LIB="install -D -m 0644"'     >> configure.local
+echo 'INSTALL_HDR="install -D -m 0644"'     >> configure.local
+echo 'INSTALL_MAN="install -D -m 0644"'     >> configure.local
+echo 'INSTALL_DATA="install -D -m 0644"'    >> configure.local
+echo 'INSTALL_LIBMANDOC=1'                  >> configure.local
+echo 'CFLAGS="-g -fPIC"'                    >> configure.local
+
+# unusual workarounds for executable on Alpine Linux
+sed -i -e 's|@echo|@/bin/echo|g' configure
+sed -i -e 's|^int dummy;$|extern int dummy;|g' compat_getline.c
+sed -i -e 's|^int dummy;$|extern int dummy;|g' compat_err.c
+
+./configure
+make
+make lib-install
+cd ${CWD}
+rm -rf mandoc.tar.gz ${SUBDIR}
